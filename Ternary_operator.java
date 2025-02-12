22e7d0af0734cff769ac9d4cf428105c83139daa
kaviyayyanar <kaviyaayyanar217@gmail.com>
2025-02-12 12:04:46 +0530

    Add files via upload

---
+++ Ternary_operator.java
@@ -0,0 +1,12 @@
+class Ternary_operator
+{
+	public static void main(String[] args) {
+	    int a=10;
+	    int b=23;
+	    int c=38;
+	    int max=(a>b && a>c)?a:(b>c)?b:c;
+	    System.out.println(max);
+	    
+	    
+	 	}
+}
