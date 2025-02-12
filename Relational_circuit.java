fe5a1871c11bcfdb65b6b7b800cdce3ca4175de9
kaviyayyanar <kaviyaayyanar217@gmail.com>
2025-02-12 11:50:55 +0530

    Add files via upload

---
+++ Relational_circuit.java
@@ -0,0 +1,20 @@
+class Relational_circuit
+{
+	public static void main(String[] args) {
+	    int a=10;
+	    int b=23;
+	    int c=38;
+	    if(a>b && a>c)
+	    {
+	        System.out.println("a is maximum");
+	    }
+	    else if(b>c){
+	        System.out.println("b is maximum");
+	    }
+	    else{
+	        System.out.println("c is maximun");
+	    }
+	    
+	    
+	 	}
+}
