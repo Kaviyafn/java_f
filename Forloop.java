e185950dea31ed29ab6dd90c3a39cd09052e78da
kaviyayyanar <kaviyaayyanar217@gmail.com>
2025-02-12 16:06:38 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+class Forloop{
+    public static void main(String[] args) {
+    int a=7;
+    for(;;){
+        if(a>=3){
+            System.out.println(a);
+        }
+        else{
+            break;}
+            a--;
+        }
+}
+}
