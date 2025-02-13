14c0612b5ac73b147b956a7dbb41da3f781b3010
kaviyayyanar <kaviyaayyanar217@gmail.com>
2025-02-13 12:08:43 +0530

    Add files via upload

---
+++ Nestedloop2.java
@@ -0,0 +1,16 @@
+
+class Nestedloop2
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    for(int i=1;i<=n;i++)
+	    {
+	    
+	        for(int k=1;k<=i;k++){
+	             System.out.print("1");
+	        }
+	        System.out.println();
+	    }
+	    
+		}
+}
